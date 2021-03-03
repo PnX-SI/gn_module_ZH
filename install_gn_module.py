59868f02fb320742487ed1284bc514b7684509d9
Julien Corny <julien_corny@natural-solutions.eu>
2021-03-03 13:49:13 +0000

    module install process : pr_zh schema and t_zh included

--- install_gn_module.py
+++ install_gn_module.py
@@ -1,8 +1,11 @@
 import subprocess
+import sys
+
+import logging
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).absolute().parent
-
+log = logging.getLogger(__name__)
 
 
 def gnmodule_install_app(gn_db, gn_app):
@@ -13,4 +16,11 @@ def gnmodule_install_app(gn_db, gn_app):
         # To run a SQL script use the gn_db parameter
         #gn_db.session.execute(open(<my_sql_data.sql>, 'r').read())
         #gn_db.session.commit()
-        pass
+        try:
+            gn_db.session.execute(
+                open(str(ROOT_DIR / "data/table_zh.sql"), "r").read()
+            )
+            gn_db.session.commit()
+        except Exception as e:
+            log.error(e)
+            raise
