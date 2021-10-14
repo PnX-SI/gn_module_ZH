da3b87d50cad9e5d5cdbece390748fb8758b6de1
amine <amine_hamouda@natural-solutions.eu>
2021-10-14 11:25:39 +0200

    FRONT: add status && evaluation data to fiche synthese

--- frontend/app/zh-details/evaluation/evaluation.component.ts
+++ frontend/app/zh-details/evaluation/evaluation.component.ts
@@ -1,10 +1,83 @@
 import { Component, Input } from "@angular/core";
 
+import { TableColumn } from "../../commonComponents/table/table-interface";
+import { EvaluationModel } from "../models/fonctionnement.model";
+
 @Component({
   selector: "zh-details-evaluation",
   templateUrl: "./evaluation.component.html",
   styleUrls: ["./evaluation.component.scss"],
 })
 export class EvaluationComponent {
-  @Input() data;
+  @Input() data: EvaluationModel;
+
+  public hydroFunctionsCols: TableColumn[] = [
+    {
+      name: "type",
+      label: "Principales fonctions hydrologiques / biogéochimiques",
+    },
+    { name: "qualification", label: "Qualifications" },
+    { name: "connaissance", label: "Connaissance" },
+  ];
+
+  public bioFunctionsCols: TableColumn[] = [
+    { name: "type", label: "Principales fonctions biologiques / écologiques" },
+    { name: "qualification", label: "Qualifications" },
+    { name: "connaissance", label: "Connaissance" },
+  ];
+
+  public patrimonialInterestsCols: TableColumn[] = [
+    { name: "type", label: "Intérêts patrimoniaux" },
+    { name: "qualification", label: "Qualifications" },
+    { name: "connaissance", label: "Connaissance" },
+  ];
+
+  public nbFauneFloreCols: TableColumn[] = [
+    {
+      name: "faunistique",
+      label: "Nombre d'espèces faunistiques patrimoniales",
+    },
+    {
+      name: "floristique",
+      label: "Nombre d'espèces floristiques patrimoniales",
+    },
+  ];
+
+  public nbHabCols: TableColumn[] = [
+    {
+      name: "nb_hab",
+      label: "Nombre d'habitats naturels humides patrimoniaux",
+    },
+    { name: "total_hab_cover", label: "Recouvrement total sur la ZH (%)" },
+  ];
+
+  public valCols: TableColumn[] = [
+    { name: "type", label: "Principales values socio-économiques" },
+    { name: "qualification", label: "Qualifications" },
+    { name: "connaissance", label: "Connaissance" },
+  ];
+
+  public propositionsCol: TableColumn[] = [
+    { name: "proposition", label: "Propositions d'actions" },
+    { name: "niveau", label: "Niveau de priorité" },
+    { name: "remarque", label: "Remarques" },
+  ];
+
+  getFauneFloreData() {
+    return [
+      {
+        faunistique: this.data.interet.faunistique,
+        floristique: this.data.interet.floristique,
+      },
+    ];
+  }
+
+  getHabitatData() {
+    return [
+      {
+        nb_hab: this.data.interet.nb_hab,
+        total_hab_cover: this.data.interet.total_hab_cover,
+      },
+    ];
+  }
 }
--- frontend/app/zh-details/statuts/statuts.component.ts
+++ frontend/app/zh-details/statuts/statuts.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { StatutsModel } from "../models/status.model";
 
 @Component({
   selector: "zh-details-statuts",
@@ -6,5 +7,28 @@ import { Component, Input } from "@angular/core";
   styleUrls: ["./statuts.component.scss"],
 })
 export class StatutsComponent {
-  @Input() data;
+  @Input() data: StatutsModel;
+
+  public regimeTableCol = [
+    { name: "status", label: "Statut" },
+    { name: "remarques", label: "Remarques" },
+  ];
+
+  public zonageTableCol = [
+    { name: "commune", label: "Commune" },
+    { name: "type_doc", label: "Type de document communal" },
+    { name: "type_classement", label: "Type de classement" },
+    { name: "remarque", label: "Remarques" },
+  ];
+
+  public instrumentsTableCol = [
+    { name: "instrument", label: "Instruments contractuels et financiers" },
+    { name: "date", label: "Date de mise en oeuvre" },
+  ];
+
+  public plansTableCol = [
+    { name: "plan", label: "Nature du plan" },
+    { name: "date", label: "Date de réalisation" },
+    { name: "duree", label: "Durée (années)" },
+  ];
 }
