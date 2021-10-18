718ae3e4ee9b385d392e4bae868dd40845b86ea9
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-18 11:07:33 +0200

    fix(backend): removed unused import (bug in 2.7.4)

--- backend/model/cards.py
+++ backend/model/cards.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.sql.expression import true
-from sqlalchemy.util.langhelpers import dependencies
+# from sqlalchemy.util.langhelpers import dependencies
 
 from geonature.utils.env import DB
 from geonature.core.ref_geo.models import LAreas
