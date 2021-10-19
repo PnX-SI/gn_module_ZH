fa07cd6503820011738b6f3ebd8dbd81de8894b5
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-19 18:11:43 +0200

    style(tab7): Add tooltip, changed labels

--- frontend/app/zh-forms/tabs/tab7/zh-form-tab7.component.ts
+++ frontend/app/zh-forms/tabs/tab7/zh-form-tab7.component.ts
@@ -44,14 +44,14 @@ export class ZhFormTab7Component implements OnInit {
   public bioFctTableCol = [
     {
       name: "function",
-      label: "Principales fonctions biologiques / écologiquess",
+      label: "Principales fonctions biologiques / écologiques",
     },
     { name: "qualification", label: "Qualifications" },
     { name: "knowledge", label: "Connaissance" },
   ];
 
   public patrimTableCol = [
-    { name: "function", label: "Intérêts patrimoniaux" },
+    { name: "function", label: "Principaux intérêts patrimoniaux" },
     { name: "qualification", label: "Qualifications" },
     { name: "knowledge", label: "Connaissance" },
   ];
