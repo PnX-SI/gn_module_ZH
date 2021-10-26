1803238f1f3c870505fc4da4d3f48fdf63e52341
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-26 11:32:09 +0200

    feat(tableComp): add inputs + outputs event
    Begin to add Inputs and Outputs to make the table component more
    generic to use it in all tabs

--- frontend/app/commonComponents/table/table.component.ts
+++ frontend/app/commonComponents/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
+import { Output, EventEmitter } from "@angular/core";
+
 import { TableColumn } from "./table-interface";
 
 @Component({
@@ -10,8 +12,21 @@ import { TableColumn } from "./table-interface";
 export class TableComponent {
   @Input() tableCols: TableColumn[];
   @Input() data: any;
+  @Input() deletable: boolean;
+  @Input() editable: boolean;
+  @Output() onDelete = new EventEmitter<string>();
+  @Output() onEdit = new EventEmitter<string>();
 
   isArray(value) {
     return Array.isArray(value);
+  }
+
+  editItem(value: string) {
+    this.onEdit.emit(value);
+  }
+
+  deleteItem(value: string) {
+    // Show a modal first
+    this.onDelete.emit(value);
   }
 }
