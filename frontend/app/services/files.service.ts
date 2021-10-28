fce68687627e1fe0140105f085892539dd504750
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-28 15:14:57 +0200

    chore(files): init files service

---
+++ frontend/app/services/files.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { ZhFile } from "./zh-form-tab8.models";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FilesService {
+  constructor() {}
+
+  filterByExtension(files: Z);
+}
