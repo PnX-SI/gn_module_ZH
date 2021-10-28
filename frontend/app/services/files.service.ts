d9f0a6394eda17954c42633b1c39feb6edcaeb64
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-28 15:40:09 +0200

    feat(files): add first functions

--- frontend/app/services/files.service.ts
+++ frontend/app/services/files.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ZhFile } from "./zh-form-tab8.models";
+import { ZhFile } from "../zh-forms/tabs/tab8/zh-form-tab8.models";
 
 @Injectable({
   providedIn: "root",
@@ -7,5 +7,9 @@ import { ZhFile } from "./zh-form-tab8.models";
 export class FilesService {
   constructor() {}
 
-  filterByExtension(files: Z);
+  filterByExtension(files: ZhFile[], extensions: string[]): ZhFile[] {
+    return files.filter((file) =>
+      extensions.includes(file.media_path.split(".").slice(-1)[0])
+    );
+  }
 }
