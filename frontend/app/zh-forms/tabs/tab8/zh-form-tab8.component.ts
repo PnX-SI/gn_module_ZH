c670ce43a268f7aae0f377534a0be832168f148c
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-29 17:36:39 +0200

    fix(tab8): removed duplicate

--- frontend/app/zh-forms/tabs/tab8/zh-form-tab8.component.ts
+++ frontend/app/zh-forms/tabs/tab8/zh-form-tab8.component.ts
@@ -50,8 +50,6 @@ export class ZhFormTab8Component implements OnInit {
   public corFilesExt = [];
   public imageFiles = {};
 
-  public fileTypeAccepted: string[] = ["application/pdf", "image/*"];
-
   constructor(
     private fb: FormBuilder,
     public ngbModal: NgbModal,
