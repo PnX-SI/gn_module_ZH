c029346effa3373191f81b51dd91952b28ec0de2
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-10-29 11:16:47 +0200

    fix(tab5): add type to taxa files; add error msg
    Add type to taxa files for http response
    Add error message if no files had been retrieved

--- frontend/app/zh-forms/tabs/tab5/zh-form-tab5.component.ts
+++ frontend/app/zh-forms/tabs/tab5/zh-form-tab5.component.ts
@@ -6,6 +6,7 @@ import { Subscription, Observable } from "rxjs";
 import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
 import { ZhDataService } from "../../../services/zh-data.service";
 import { TabsService } from "../../../services/tabs.service";
+import { TaxaFile } from "./zh-form-tab5.models";
 
 @Component({
   selector: "zh-form-tab5",
@@ -716,15 +717,26 @@ export class ZhFormTab5Component implements OnInit {
     this._dataService
       .getTaxa(this.currentZh.id)
       .toPromise()
-      .then((res) => {
-        const files = res.file_names.map((file) =>
-          file.replace(/^.*[\\\/]/, "")
-        );
-        const msg = `Les fichiers suivants ont été générés ${files.join(", ")}`;
-        this._toastr.success(msg, "", {
-          disableTimeOut: true, // to be sure the user sees the toast
-          closeButton: true,
-        });
+      .then((res: TaxaFile) => {
+        if (res.file_names.length == 0) {
+          const msg =
+            "Aucun fichier n'a été généré car aucune espèce n'a été trouvée dans la zone humide";
+          this._toastr.error(msg, "", {
+            disableTimeOut: true, // to be sure the user sees the toast
+            closeButton: true,
+          });
+        } else {
+          const files = res.file_names.map((file) =>
+            file.replace(/^.*[\\\/]/, "")
+          );
+          const msg = `Les fichiers suivants ont été générés ${files.join(
+            ", "
+          )}`;
+          this._toastr.success(msg, "", {
+            disableTimeOut: true, // to be sure the user sees the toast
+            closeButton: true,
+          });
+        }
       })
       .finally(() => {
         this.taxaLoading = false;
---
+++ frontend/app/zh-forms/tabs/tab5/zh-form-tab5.models.ts
@@ -0,0 +1,3 @@
+export type TaxaFile = {
+  file_names: string[];
+};
