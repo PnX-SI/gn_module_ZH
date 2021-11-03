1c747247f9aec510f5a4215477ce8c42ce9abcf0
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-03 16:51:19 +0100

    feat(tab0): delete geom when contained
    Add check if the error returned is a "contained geometry error"
    and delete the geom in this case.
    Improved the error translator service

--- frontend/app/services/error-translator.service.ts
+++ frontend/app/services/error-translator.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 type error = {
   api: string;
   front: string;
+  id: number;
 };
 
 @Injectable({
@@ -15,20 +16,32 @@ export class ErrorTranslatorService {
     {
       api: "400 Bad Request: polygon_contained_in_zh", // error returned by backend
       front: "La geometrie est contenue entièrement dans une autre zone humide", //error to show to the user
+      id: 1,
     },
     {
       api: "ZH_main_name_already_exists", // error returned by backend
       front:
         "Impossible de créer un identifiant unique avec les informations entrées. Le nom est peut-être déjà pris ?", //error to show to the user
+      id: 2,
     },
   ];
 
-  getFrontError(errorMsg: string): string {
-    console.log("error:", errorMsg);
+  getError(errorMsg: string): error {
     const frontError: error = this.errors.filter(
       (item) => item.api == errorMsg
     )[0];
+    return frontError;
+  }
+
+  getFrontError(errorMsg: string): string {
+    console.log("error:", errorMsg);
+    const frontError: error = this.getError(errorMsg);
     console.log(frontError);
     return frontError ? frontError.front : "Erreur inconnue";
+  }
+
+  getErrorId(errorMsg: string): number {
+    const frontError: error = this.getError(errorMsg);
+    return frontError ? frontError.id : 999999;
   }
 }
