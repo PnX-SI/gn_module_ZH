6286a533a62b5d54b2b05994e0b095b2c1ba5548
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-04 16:36:38 +0000

    fix(api): intersection mixed geom
    Correction error of mixed geometries for intersections in set_geom function

--- backend/geometry.py
+++ backend/geometry.py
@@ -2,16 +2,14 @@ from flask import abort
 
 from geonature.utils.env import DB
 
-from sqlalchemy import func
+from sqlalchemy import func, cast
 
 from geoalchemy2.shape import to_shape
+from geoalchemy2.types import Geography, Geometry
 
 from .model.zh_schema import TZH
 
 from .api_error import ZHApiError
-
-
-import pdb
 
 
 def set_geom(geometry, id_zh=None):
@@ -22,13 +20,14 @@ def set_geom(geometry, id_zh=None):
     is_intersected = False
     for zh in q_zh:
         if zh.id_zh != id_zh:
-            if DB.session.query(func.ST_Intersects(polygon, zh.geom)).scalar():
+            zh_geom = DB.session.query(func.ST_GeogFromWKB(func.ST_AsEWKB(zh.geom))).scalar()
+            polygon_geom = DB.session.query(func.ST_GeogFromWKB(func.ST_AsEWKB(polygon))).scalar()
+            if DB.session.query(func.ST_Intersects(polygon_geom, zh_geom)).scalar():
                 is_intersected = True
-            if DB.session.query(func.ST_Contains(zh.geom, polygon)).scalar():
-                abort(
-                    400, 'polygon_contained_in_zh')
+            if DB.session.query(func.ST_Intersects(zh_geom, polygon_geom)).scalar():
+                abort(400, 'polygon_contained_in_zh')
             intersect = DB.session.query(
-                func.ST_Difference(polygon, zh.geom))
+                func.ST_Difference(polygon_geom, zh_geom))
             polygon = DB.session.query(func.ST_GeomFromText(
                 to_shape(intersect.scalar()).to_wkt())).one()[0]
     return {
