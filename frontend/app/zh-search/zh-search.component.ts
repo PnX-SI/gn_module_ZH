1ef1d80f6b5a90109e526484255e5a801441adbf
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-08 11:02:18 +0100

    fix(filters): forgot attribute declaration

--- frontend/app/zh-search/zh-search.component.ts
+++ frontend/app/zh-search/zh-search.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from "@angular/core";
 export class ZhSearchComponent implements OnInit {
   @Input() forms: any;
 
+  public selectedDepartment: any;
+
   constructor() {}
 
   ngOnInit() {}
