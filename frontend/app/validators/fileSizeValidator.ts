f8d1c70ee6af7afbac8f9a43c08411e33f235378
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-16 11:26:25 +0100

    feat(tab8): maxSize is now optional in validator
    In fileSizeValidator, maxSize is now optional

--- frontend/app/validators/fileSizeValidator.ts
+++ frontend/app/validators/fileSizeValidator.ts
@@ -1,14 +1,15 @@
 import { FormControl } from "@angular/forms";
-export function fileSizeValidator(maxSize: number, maxSizePdf?) {
+export function fileSizeValidator(maxSizePhoto?: number, maxSizePdf?: number) {
   return function (control: FormControl) {
     const file: File = control.value;
     if (file) {
+      let maxSize: number = maxSizePhoto;
       if (file.type == "application/pdf") {
         maxSize = maxSizePdf;
       }
       const fileSize: number = file.size;
       const fileSizeInKB: number = Math.round(fileSize / 1024);
-      if (fileSizeInKB >= maxSize) {
+      if (maxSize && fileSizeInKB >= maxSize) {
         return {
           fileSizeValidator: true,
         };
