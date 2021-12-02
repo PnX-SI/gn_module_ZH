878890e10ebb512fead6412a403b6c0b390f289b
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-12-02 16:35:53 +0100

    feat(pdf): add map generation
    With a library called staticmap: not sure if it is the best option
    but it kind of works...

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -32,6 +32,13 @@ from sqlalchemy.orm.exc import NoResultFound
 import geoalchemy2
 from datetime import datetime as dt, timezone
 
+from io import BytesIO
+import base64
+try:
+    from staticmap import StaticMap, Line
+except ImportError:
+    print('cannot import staticmap, map generation in pdf will be unavailable')
+
 from pypn_habref_api.models import (
     Habref,
     CorespHab
@@ -869,6 +876,19 @@ def returnUserCruved(info_role):
     )
     return user_cruved
 
+def gen_map(coordinates):
+    if coordinates:
+        m = StaticMap(width=600, height=300)
+        poly = Line(coordinates, 'blue', 4)
+        m.add_line(poly)
+        image = m.render()
+        with BytesIO() as output:
+            image.save(output, format='PNG')
+            contents =  base64.b64encode(output.getvalue())
+        return "data:image/jpeg;base64," + contents.decode()
+    return None
+
+
 @blueprint.route("/export_pdf/<int:id_zh>", methods=["GET"])
 @permissions.check_cruved_scope("R", module_code="ZONES_HUMIDES")
 def download(id_zh: int):
@@ -877,6 +897,12 @@ def download(id_zh: int):
     """
     filename = "rapport.pdf"
     dataset = get_complete_card(id_zh)
+
+    try:
+        dataset['map'] = gen_map(dataset.get('geometry', {}).get('coordinates', [[]])[0])
+    except Exception as e:
+        print('Cannot generate the map inside the pdf... Continuing')
+        
     pdf_file = fm.generate_pdf("fiche_template_pdf.html", dataset, filename)
     pdf_file_posix = Path(pdf_file)
     return send_file(pdf_file_posix, as_attachment=True)
