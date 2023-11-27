6ec9cc32fd5e43ed88f7cb2110094878be816a65
cen-cgeier <colas.geier@cen-isere.org>
2023-11-27 16:00:41 +0100

    fix lint

--- backend/gn_module_zh/migrations/59ae9451ef41_add_index_geom.py
+++ backend/gn_module_zh/migrations/59ae9451ef41_add_index_geom.py
@@ -10,12 +10,13 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '59ae9451ef41'
-down_revision = '335377e24a52'
+revision = "59ae9451ef41"
+down_revision = "335377e24a52"
 branch_labels = None
 depends_on = None
 
 SCHEMA = "pr_zh"
+
 
 def upgrade():
     op.create_index(
@@ -23,25 +24,32 @@ def upgrade():
         table_name="t_zh",
         columns=["geom"],
         schema=SCHEMA,
-        postgresql_using='gist')
-    
+        postgresql_using="gist",
+    )
+
     op.create_index(
         "index_t_river_basin_geom",
         table_name="t_river_basin",
-        columns=["geom"], schema=SCHEMA,
-        postgresql_using='gist')
-    
+        columns=["geom"],
+        schema=SCHEMA,
+        postgresql_using="gist",
+    )
+
     op.create_index(
         "index_t_fct_area_geom",
         table_name="t_fct_area",
-        columns=["geom"], schema=SCHEMA,
-        postgresql_using='gist')
-    
+        columns=["geom"],
+        schema=SCHEMA,
+        postgresql_using="gist",
+    )
+
     op.create_index(
         "index_t_hydro_area_geom",
         table_name="t_hydro_area",
-        columns=["geom"], schema=SCHEMA,
-        postgresql_using='gist')
+        columns=["geom"],
+        schema=SCHEMA,
+        postgresql_using="gist",
+    )
 
 
 def downgrade():
