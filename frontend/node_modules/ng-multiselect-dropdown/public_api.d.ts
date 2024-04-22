23d80e7ce879e98a8f49302c5d07175d4cebc4dd
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2024-04-22 17:53:51 +0200

    feat: purge ng_multiselect_dropdown

--- frontend/node_modules/ng-multiselect-dropdown/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { MultiSelectComponent } from './multiselect.component';
-export { NgMultiSelectDropDownModule } from './ng-multiselect-dropdown.module';
-export { IDropdownSettings } from './multiselect.model';
