105a8fe60e3b04fdd8154083272d7e6396bc07ad
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2024-09-27 16:39:56 +0200

    chore: adjust alembic revision name

--- backend/gn_module_zh/migrations/72a8378567pa_update_main_id_rb.py
+++ backend/gn_module_zh/migrations/72a8378567pa_update_main_id_rb.py
@@ -1,4 +1,4 @@
-"""add_main_id_rb_column
+"""update_main_id_rb
 
 Revision ID: 72a8378567pa
 Revises: 58ab8aba8512
@@ -20,20 +20,20 @@ depends_on = None
 def upgrade():
     op.execute(
         """
-        UPDATE pr_zh.t_zh tzh 
-        SET main_id_rb = 
+        UPDATE pr_zh.t_zh tzh
+        SET main_id_rb =
         (
-            SELECT id_rb 
+            SELECT id_rb
             FROM (
                 SELECT
-                    czr.id_rb AS id_rb, 
+                    czr.id_rb AS id_rb,
                     ST_Area(ST_Intersection(
                         ST_GeomFromText(ST_AsText((SELECT geom FROM pr_zh.t_zh WHERE id_zh = tzh.id_zh ))),
                         ST_GeomFromText(ST_AsText(trb.geom))
-                    )) AS areas									
+                    )) AS areas
                 FROM pr_zh.cor_zh_rb czr
                 LEFT JOIN pr_zh.t_river_basin trb ON trb.id_rb = czr.id_rb
-                WHERE czr.id_zh = tzh.id_zh 
+                WHERE czr.id_zh = tzh.id_zh
                 ORDER BY areas DESC
                 LIMIT 1
             ) AS a
@@ -45,7 +45,7 @@ def upgrade():
 def downgrade():
     op.execute(
         """
-        UPDATE pr_zh.t_zh tzh 
+        UPDATE pr_zh.t_zh tzh
         SET main_id_rb = null
         """
     )
